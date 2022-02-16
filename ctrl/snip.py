649d941cf7b3cf990a37da8dd8d99bf8cbdad33e
Dean Harding <dean@codeka.com.au>
2022-02-16 14:39:10 -0800

    Removed some stuff.

--- ctrl/snip.py
+++ ctrl/snip.py
@@ -12,30 +12,6 @@ def _isValidSlug(slug):
   return SLUG_REGEX.match(slug) != None
 
 
-def createSnip(blobKey):
-  slug = ""
-  while True:
-    slug = ""
-    for _ in xrange(12):
-      slug += random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    # TODO: check whether the slug already exists (unlikely but possible)
-    break
-
-  img = images.Image(blob_key=blobKey)
-  img.im_feeling_lucky() # we have to do a transform in order to get dimensions
-  img.execute_transforms()
-
-  snip = model.snip.Snip()
-  snip.slug = slug
-  snip.blob = blobKey
-  snip.width = img.width
-  snip.height = img.height
-  snip.put()
-
-  memcache.set('snip:%s' % slug, snip)
-
-  return snip
-
 def getSnipPath(slug):
   if not _isValidSlug(slug):
     return None
